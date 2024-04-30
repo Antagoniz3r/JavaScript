705a1aa4ee49843b169bacf331e0c464c8864e64
Antagoniz3r <marcnijboer@gmail.com>
2024-04-30 21:06:34 +0200

    First Commit

---
+++ Website/index.js
@@ -0,0 +1,13 @@
+//console.log(`Hello`);
+
+//window.alert(`Sheik Supremacy!`);
+
+// This is a comment: Clean code
+
+//document.getElementById("MyH1") = `Sheik`;
+//document.getElementById("MyP1") = `Sheik error`;
+
+let x;
+x = 240; 
+
+console.log(x);
